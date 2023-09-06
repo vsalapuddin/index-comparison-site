6df76913a0b389fb330c515168451a0801a98414
Salapuddin <vsalapuddin@ufl.edu>
2023-09-05 22:30:01 -0400

    added page component

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center">
+      <div className="flex flex-col h-screen items-center justify-between">
+        <div></div>
+        <div>
+          <h2 className="text-sm uppercase text-gray-400 pb-2 tracking-[5px] md:tracking-[15px] lg:tracking-[15px] text-center mb-4">
+            Test
+          </h2>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
