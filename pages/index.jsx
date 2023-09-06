d1c90c25906354f789b1f53e452a444b4aa861c3
Salapuddin <vsalapuddin@ufl.edu>
2023-09-06 00:41:34 -0400

    switch to chart.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,34 @@
-import Head from "next/head";
-import dynamic from "next/dynamic";
+// App.js
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { Data } from "../utils/Data";
+import LineChart from "./Chart/lineChart";
 
-const Chart = dynamic(() => import("./Chart/CanvasJSChart"), { ssr: false });
+Chart.register(CategoryScale);
 
-export default function Home() {
+export default function App() {
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
-      <title>Index</title>
-      <Chart />
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 no-scrollbar">
+      <LineChart chartData={chartData} />
     </div>
   );
 }
